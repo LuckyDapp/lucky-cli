7426a777d9f3f59c45804d3cff87daa34c61eb61
guigou <guigoucrypto@gmail.com>
2024-04-09 19:41:12 +0200

    Fic the error management when query the phat contract

--- src/raffle.ts
+++ src/raffle.ts
@@ -12,8 +12,13 @@ export async function callRafflePhatContract() : Promise<void>{
 
     if (result.isOk){
         const value : string = output?.toString() ?? '';
-        const tx = JSON.parse(value).ok;
-        console.log('Tx for the raffle: %s', tx);
+        console.log('Result: %s', value);
+        const res = JSON.parse(value).ok;
+        if (res.err){
+            console.log('Error: %s', res.err);
+            return Promise.reject("ERROR when start the raffle");
+        }
+        console.log('Tx for the raffle: %s', res.ok);
         return;
     }
     return Promise.reject("ERROR when start the raffle " + result.asErr);
